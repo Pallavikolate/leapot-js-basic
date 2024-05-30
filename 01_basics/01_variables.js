d8b5f6805c1b6678f7a555f5465dd147feb83195
Pallavikolate <102782146+Pallavikolate@users.noreply.github.com>
2024-05-30 08:35:20 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const id=23
+console.log(id);
+
+let name="Pallavi"
+console.log(name);
+
+var age=24
+console.log(age);
+
+/*
+prefer not to use var beacouse of issue in block scope and functional scope
+*/
+
+let email="pallavi@gmail.com"
+let password="23243"
+var isClass=true
+console.table([email,password,isClass])
