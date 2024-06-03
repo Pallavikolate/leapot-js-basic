a28e8712d9c75cb899efa036e2f8115f3329f3ce
Pallavikolate <102782146+Pallavikolate@users.noreply.github.com>
2024-06-03 09:51:03 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+const gameName="Hello";
+const countRepo="world";
+//console.log(gameName + countRepo + " 69");
+
+//console.log(`game name is ${gameName.toLowerCase()} and famous in ${countRepo} also value is 69`);
+
+const myName=new String("Pallavi")
+// console.log(myName);
+// console.log(myName[0]);
+// console.log(myName.__proto__);
+
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+
+// console.log(myName.substring(0,5));
+// console.log(myName.slice(2,6));
+
+const anotherOne= myName.split('a')
+//console.log(anotherOne);
+
+const newString= myName.includes("a")
+//console.log(newString);
+
+console.log(myName.replace('a','k'));
+
+
