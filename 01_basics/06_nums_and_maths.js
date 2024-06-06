e558ec8bb5cf81b350b2418455c78eec212496b9
Pallavikolate <102782146+Pallavikolate@users.noreply.github.com>
2024-06-06 11:23:20 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score=400
+//console.log(score);
+
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum=234.3343
+//console.log(otherNum.toPrecision(1));
+
+const hundred=1000000
+//console.log(hundred.toLocaleString('en-IN'));
+
+//*************************************   MATHS     *****************
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(34.89));
+// console.log(Math.ceil(34.89));
+// console.log(Math.min(4,7,2,8,5));
+// console.log(Math.max(4,7,2,8,5));
+
+// console.log(Math.random());
+// console.log(Math.random()*10+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
